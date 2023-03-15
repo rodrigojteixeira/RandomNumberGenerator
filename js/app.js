79a834a31946fc852ad13dfff73e6817bbbb3cf4
Rodrigo Teixeira <rodrigojoseteixeira92@gmail.com>
2023-03-15 10:12:05 -0300

    Criando a função de click

--- js/app.js
+++ js/app.js
@@ -1,2 +1,8 @@
 const button = document.getElementById('generate');
 
+button.addEventListener('click', function() {
+    const min = parseInt(document.getElementById('min').value);
+    const max = parseInt(document.getElementById('max').value);
+
+}
+
