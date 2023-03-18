a45d2a3e91874e9f65410c8b7670ec4d02d8a4e0
Rodrigo Teixeira <rodrigojoseteixeira92@gmail.com>
2023-03-17 23:22:32 -0300

    variavel resultado

--- js/app.js
+++ js/app.js
@@ -4,4 +4,6 @@ button.addEventListener('click', function() {
     const min = parseInt(document.getElementById('min').value);
     const max = parseInt(document.getElementById('max').value);
 
+    let result = Math.floor(Math.random() * (max - min + 1)) + min;
+    
 }
